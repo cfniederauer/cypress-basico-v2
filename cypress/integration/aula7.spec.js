0001bf60b861fe42a0bfdb4084591540d27dd701
Niederauer <cniedera@ad.esi.adp.com>
2022-07-29 10:33:10 -0300

    Breaking on purpose

--- cypress/integration/aula7.spec.js
+++ cypress/integration/aula7.spec.js
@@ -13,7 +13,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#privacy a')
         .invoke('removeAttr', 'target').click()
         
-        cy.contains('Talking About Testing').should('be.visible')
+        cy.contains('Talking About Testingui').should('be.visible')
         })
     
     })
