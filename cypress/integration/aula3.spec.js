c02638a461f4df7b9713d969974dba644a912fd7
Niederauer <cniedera@ad.esi.adp.com>
2022-07-29 11:06:43 -0300

    Fixing issues in aula3 and aula7

--- cypress/integration/aula3.spec.js
+++ cypress/integration/aula3.spec.js
@@ -9,7 +9,7 @@ describe('Central de Atendimento ao Cliente TAT', function() {
         cy.get('#product').select('YouTube').should('have.value', 'youtube')
     })
     it('seleciona um produto (Mentoria) por seu valor (value)', ()=>{
-        cy.get('#product').select('mentoria').should('have.value', 'mentoriaa')
+        cy.get('#product').select('mentoria').should('have.value', 'mentoria')
     })
     it('seleciona um produto (Blog) por seu índice', ()=>{
         cy.get('#product').select(1).should('have.value', 'blog')
