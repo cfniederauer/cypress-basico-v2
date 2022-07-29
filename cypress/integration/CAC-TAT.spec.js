4580b32cf1aae78e3dc00d4a8bcc79d9a6b59ce0
Niederauer <cniedera@ad.esi.adp.com>
2022-07-29 18:07:43 -0300

    Including .clock() and .tick() to not visible messages

--- cypress/integration/CAC-TAT.spec.js
+++ cypress/integration/CAC-TAT.spec.js
@@ -11,23 +11,31 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it.only('Preencher formulario', function() {
         const longText = 'Eu nao preciso de ajuda!, Eu nao preciso de ajuda!,Eu nao preciso de ajuda!,Eu nao preciso de ajuda!,Eu nao preciso de ajuda!,Eu nao preciso de ajuda!,Eu nao preciso de ajuda!,Eu nao preciso de ajuda!'
 
+        cy.clock()
+        
         cy.get('#firstName').should('be.visible').type('Caio').should('have.value', 'Caio')
         cy.get('#lastName').should('be.visible').type('Niederauer').should('have.value', 'Niederauer')
         cy.get('#email').should('be.visible').type('caio@test.com').should('have.value', 'caio@test.com')
         cy.get('#open-text-area').should('be.visible').type(longText, {delay: 0}).should('have.value', longText)
         cy.contains('button', 'Enviar').click()
         cy.get('.success').should('be.visible')
+        cy.tick(3000) 
+        cy.get('.success').should('not.be.visible')
+
         
     })
     it('exibe mensagem de erro ao submeter o formulário com um email com formatação inválida', function() {
         const longText = 'Eu nao preciso de ajuda!, Eu nao preciso de ajuda!,Eu nao preciso de ajuda!,Eu nao preciso de ajuda!,Eu nao preciso de ajuda!,Eu nao preciso de ajuda!,Eu nao preciso de ajuda!,Eu nao preciso de ajuda!'
 
+        cy.clock()
         cy.get('#firstName').should('be.visible').type('Caio').should('have.value', 'Caio')
         cy.get('#lastName').should('be.visible').type('Niederauer').should('have.value', 'Niederauer')
         cy.get('#email').should('be.visible').type('caiotest.com')
         cy.get('#open-text-area').should('be.visible').type(longText, {delay: 0}).should('have.value', longText)
         cy.get('button[type="submit"]').click()
         cy.get('.error').should('be.visible')
+        cy.tick(3000) 
+        cy.get('.error').should('not.be.visible')
     })
 
     it('campo telefone só aceita números', function() {
@@ -37,13 +45,16 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     it.only('exibe mensagem de erro quando o telefone se torna obrigatório mas não é preenchido antes do envio do formulário', function(){
 
         const longText = 'Eu nao preciso de ajuda!, Eu nao preciso de ajuda!,Eu nao preciso de ajuda!,Eu nao preciso de ajuda!,Eu nao preciso de ajuda!,Eu nao preciso de ajuda!,Eu nao preciso de ajuda!,Eu nao preciso de ajuda!'
-
+        cy.clock()
         cy.get('#firstName').should('be.visible').type('Caio').should('have.value', 'Caio')
         cy.get('#lastName').should('be.visible').type('Niederauer').should('have.value', 'Niederauer')
         cy.get('#email').should('be.visible').type('caio@test.com').should('have.value', 'caio@test.com')
         cy.get('#phone-checkbox').check()
         cy.get('#open-text-area').should('be.visible').type(longText, {delay: 0}).should('have.value', longText)
         cy.get('button[type="submit"]').click()
+        cy.get('.error').should('be.visible')
+        cy.tick(3000) 
+        cy.get('.error').should('not.be.visible')
     })
     it('preenche e limpa os campos', function(){
         cy.get('#firstName').type('Caio').should('have.value', 'Caio')
@@ -51,14 +62,20 @@ describe('Central de Atendimento ao Cliente TAT', function() {
     })
 
     it('exibe mensagem de erro ao submeter o formulário sem preencher os campos obrigatórios', function() {
+        cy.clock()
         cy.get('button[type="submit"]').click()
         cy.get('.error').should('be.visible')
+        cy.tick(3000)
+        cy.get('error').should('not.be.visible')
 
     })
 
     it('envia o formuário com sucesso usando um comando customizado', function(){
+        cy.clock()
         cy.fillMandatoryFieldsAndSubmit('Caio', 'Niederauer', 'caio@gmail.com')
         cy.get('.success').should('be.visible')
+        cy.tick(3000)
+        cy.get('success').should('not.be.visible')
     })
 
 
